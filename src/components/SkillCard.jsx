df583d73ae8d77563baed8f861dccc21d9d98e6c
ricocatford <s7funoob@gmail.com>
2023-03-03 23:37:21 +0000

    Add big refactoring

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -10,7 +10,7 @@ const SkillCard = (props) => {
             {skills.map((skill) => (
                 <Col xs={6} sm={6} md={3} lg={2}>
                     <div className="container mx-auto">
-                        <img src={skill.img} alt={skill.name} className="image"/>
+                        <img src={skill.image} alt={skill.name} className="image"/>
                         <p className="text-center">
                             {skill.name}
                         </p>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,16 +2,6 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import html from "../assets/images/html.png";
-import css from "../assets/images/css.png";
-import js from "../assets/images/js.png";
-import python from "../assets/images/python.png";
-import flask from "../assets/images/flask.png";
-import django from "../assets/images/django.png";
-import react from "../assets/images/react.png";
-import nodejs from "../assets/images/nodejs.png";
-import git from "../assets/images/git.png";
-
 import "../assets/css/style.css"
 
 const About = () => {
@@ -20,7 +10,7 @@ const About = () => {
             <Row>
                 <Col className="col-sm-12 text-center">
                     <h2 className="heading">
-                        About Rico
+                        Who is Rico?
                     </h2>
                 </Col>
             </Row>
@@ -46,78 +36,7 @@ const About = () => {
                 </Col>
             </Row>
             <Row>
-                <Col xs={6} sm={4} md={3} lg={2}>
-                    <div className="skill-container mx-auto">
-                        <img src={html} alt="HTML" className="skill-image"/>
-                        <p className="text-center">
-                            HTML
-                        </p>
-                    </div>
-                </Col>
-                <Col xs={6} sm={4} md={3} lg={2}>
-                    <div className="skill-container mx-auto">
-                        <img src={css} alt="CSS" className="skill-image" />
-                        <p className="text-center">
-                            CSS
-                        </p>
-                    </div>
-                </Col>
-                <Col xs={6} sm={4} md={3} lg={2}>
-                    <div className="skill-container mx-auto">
-                        <img src={js} alt="JavaScript" className="skill-image" />
-                        <p className="text-center">
-                            JavaScript
-                        </p>
-                    </div>
-                </Col>
-                <Col xs={6} sm={4} md={3} lg={2}>
-                    <div className="skill-container mx-auto">
-                        <img src={python} alt="Python" className="skill-image"/>
-                        <p className="text-center">
-                            Python
-                        </p>
-                    </div>
-                </Col>
-                <Col xs={6} sm={4} md={3} lg={2}>
-                    <div className="skill-container mx-auto">
-                        <img src={flask} alt="Flask" className="skill-image"/>
-                        <p className="text-center">
-                            Flask
-                        </p>
-                    </div>
-                </Col>
-                <Col xs={6} sm={4} md={3} lg={2}>
-                    <div className="skill-container mx-auto">
-                        <img src={django} alt="Django" className="skill-image"/>
-                        <p className="text-center">
-                            Django
-                        </p>
-                    </div>
-                </Col>
-                <Col xs={6} sm={4} md={3} lg={2}>
-                    <div className="skill-container mx-auto">
-                        <img src={react} alt="ReactJS" className="skill-image"/>
-                        <p className="text-center">
-                            ReactJS
-                        </p>
-                    </div>
-                </Col>
-                <Col xs={6} sm={4} md={3} lg={2}>
-                    <div className="skill-container mx-auto">
-                        <img src={nodejs} alt="NodeJS" className="skill-image"/>
-                        <p className="text-center">
-                            NodeJS
-                        </p>
-                    </div>
-                </Col>
-                <Col xs={6} sm={4} md={3} lg={2}>
-                    <div className="skill-container mx-auto">
-                            <img src={git} alt="Git" className="skill-image"/>
-                            <p className="text-center">
-                                Git
-                            </p>
-                    </div>
-                </Col>
+
             </Row>
         </Container>
     );
