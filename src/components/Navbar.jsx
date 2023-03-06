f92097d4e75c1ebf558e3f045bde3bcf97a74173
ricocatford <s7funoob@gmail.com>
2023-03-06 12:31:19 +0000

    Add .gitignore, delete node_modules directory

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,24 +11,24 @@ const CustomNavbar = () => {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top" expanded={expanded}>
+    <Navbar bg="dark" variant="dark" expand="lg" expanded={expanded}>
       <Container>
-        <Navbar.Brand as={Link} to={"/"}>
+        <Navbar.Brand as={Link} to="/">
           <img src="images/new-logo.png" alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to={"/"} onClick={() => setExpanded(false)} className="link text-uppercase">
+            <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)} className="link text-uppercase">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to={"portfolio"} onClick={() => setExpanded(false)} className="link text-uppercase">
+            <Nav.Link as={Link} to="portfolio" onClick={() => setExpanded(false)} className="link text-uppercase">
               Portfolio
             </Nav.Link>
-            <Nav.Link as={Link} to={"about"} onClick={() => setExpanded(false)} className="link text-uppercase">
+            <Nav.Link as={Link} to="about" onClick={() => setExpanded(false)} className="link text-uppercase">
               About
             </Nav.Link>
-            <Nav.Link as={Link} to={"contact"} onClick={() => setExpanded(false)} className="link text-uppercase">
+            <Nav.Link as={Link} to="contact" onClick={() => setExpanded(false)} className="link text-uppercase">
               Contact
             </Nav.Link>
           </Nav>
