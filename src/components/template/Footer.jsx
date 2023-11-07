e2143ba919606020341a6281cd2aaca3a447e6ae
ricocatford <s7funoob@gmail.com>
2023-11-07 11:29:20 +0000

    fix: footer tag

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -2,13 +2,13 @@ import "../../assets/css/Footer.css";
 
 export default function Footer() {
     return (
-        <div className="container--fluid">
+        <footer className="container--fluid">
             <div className="footer__container">
                 <p className="footer__text text-center">
                     &copy; Copyright 2023 - Design and content by Ricardo
                     Albarenque
                 </p>
             </div>
-        </div>
+        </footer>
     );
 }
